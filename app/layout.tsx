18d5bc6517121e20643b373b160bf9055fe67329
duc-lt-mor <duc.lt@mor.com.vn>
2024-01-10 16:43:00 +0700

    sign in ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ import Loader from "@/components/common/Loader";
 
 import Sidebar from "@/components/Sidebar";
 import Header from "@/components/Header";
+import { redirect } from "next/navigation";
+import SignIn from "./signin/page";
 
 export default function RootLayout({
   children,
@@ -16,47 +18,56 @@ export default function RootLayout({
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   const [loading, setLoading] = useState<boolean>(true);
+  const [token, setToken] = useState("");
 
   useEffect(() => {
+    // TODO: add real auth
+    setToken(localStorage.getItem("token") ?? "");
     setTimeout(() => setLoading(false), 1000);
   }, []);
 
   return (
     <html lang="en">
       <body suppressHydrationWarning={true}>
-        <div className="dark:bg-boxdark-2 dark:text-bodydark">
-          {loading ? (
-            <Loader />
-          ) : (
-            <div className="flex h-screen overflow-hidden">
-              {/* <!-- ===== Sidebar Start ===== --> */}
-              <Sidebar
-                sidebarOpen={sidebarOpen}
-                setSidebarOpen={setSidebarOpen}
-              />
-              {/* <!-- ===== Sidebar End ===== --> */}
-
-              {/* <!-- ===== Content Area Start ===== --> */}
-              <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-                {/* <!-- ===== Header Start ===== --> */}
-                <Header
+        {token ? (
+          <div className="dark:bg-boxdark-2 dark:text-bodydark">
+            {loading ? (
+              <Loader />
+            ) : (
+              <div className="flex h-screen overflow-hidden">
+                {/* <!-- ===== Sidebar Start ===== --> */}
+                <Sidebar
                   sidebarOpen={sidebarOpen}
                   setSidebarOpen={setSidebarOpen}
                 />
-                {/* <!-- ===== Header End ===== --> */}
+                {/* <!-- ===== Sidebar End ===== --> */}
 
-                {/* <!-- ===== Main Content Start ===== --> */}
-                <main>
-                  <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-                    {children}
-                  </div>
-                </main>
-                {/* <!-- ===== Main Content End ===== --> */}
+                {/* <!-- ===== Content Area Start ===== --> */}
+                <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+                  {/* <!-- ===== Header Start ===== --> */}
+                  <Header
+                    sidebarOpen={sidebarOpen}
+                    setSidebarOpen={setSidebarOpen}
+                  />
+                  {/* <!-- ===== Header End ===== --> */}
+
+                  {/* <!-- ===== Main Content Start ===== --> */}
+                  <main>
+                    <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                      {children}
+                    </div>
+                  </main>
+                  {/* <!-- ===== Main Content End ===== --> */}
+                </div>
+                {/* <!-- ===== Content Area End ===== --> */}
               </div>
-              {/* <!-- ===== Content Area End ===== --> */}
-            </div>
-          )}
-        </div>
+            )}
+          </div>
+        ) : (
+          <div className="flex justify-center content-center items-center h-screen">
+            <SignIn />
+          </div>
+        )}
       </body>
     </html>
   );
--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Admin | Sign In",
-};
+// export const metadata: Metadata = {
+//   title: "Admin | Sign In",
+// };
 
 const SignIn: React.FC = () => {
   return (
-    <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-      <div className="w-full border-stroke dark:border-strokedark xl:w-1/2 xl:border-l-2">
+    <div className="rounded-sm w-1/3 center border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+      <div className="w-full border-stroke dark:border-strokedark xl:w-full xl:border-l-2">
         <div className="w-full p-4 sm:p-12.5 xl:p-17.5">
           <h2 className="mb-9 text-2xl font-bold text-black dark:text-white sm:text-title-xl2">
             Sign in
